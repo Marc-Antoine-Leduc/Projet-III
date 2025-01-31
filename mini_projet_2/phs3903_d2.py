a4a07e2b64a0d582e238ec7ddec1d33c9917e4a3
Marc-Antoine-Leduc <leduc.marc.antoine@gmail.com>
2025-01-31 10:50:17 -0500

    Add files via upload
    Question 3

--- mini_projet_2/phs3903_d2.py
+++ mini_projet_2/phs3903_d2.py
@@ -10,34 +10,68 @@ from scipy import stats
 g = 9.81     # Champ gravitationnel (m²/s)
 m = 1.000    # Masse du pendule (kg)
 L = 1.000    # Longueur du câble (m)
-beta = 0.04  # Constante d'amortissement (1/s)
+beta = 0.1  # Constante d'amortissement (1/s)
+pi = np.pi
 
 # Conditions initiales
-theta0 = 30         # Position initiale (rad)
-omega0 = 1          # Vitesse inititale (rad/s)
+theta0 = pi/6         # Position initiale (rad)
+omega0 = 5          # Vitesse inititale (rad/s)
 
 # Paramètres généraux de simulation
-tf = 1             # Temps final (s)
-dt0 = 1.000        # Pas de temps le plus élevé (s)
+tf = 10             # Temps final (s)
+T = 2 * np.pi * np.sqrt(L / g)
+dt0 = T / 100  # Pas de temps le plus élevé (s)
 
 # Boucle sur le nombre de simulations
 K = 5                                       # Nombre de simulations
-dt_val = [1, 2, 3, 4, 5]                    # Vecteur des pas de temps pour chaque simulation
+dt_val = [dt0, 2*dt0, 4*dt0, 8*dt0, 16*dt0] # Vecteur des pas de temps pour chaque simulation
 thetaf = np.zeros(K)                        # Vecteur des positions finales pour chaque simulation
+position_finale = []                        # Initialiser un vecteur position finale
+firstTry = True
 
 for k in range(0,K):
 # Paramètres spécifiques de la simulation
-    dt = dt_val[k]               # Pas de temps de la simulation
-    N = 100  # Nombre d'itérations (conseil : s'assurer que dt soit un multiple entier de tf)
+    dt = dt_val[k]    # Pas de temps de la simulation
+    N = int(tf / dt)  # Nombre d'itérations (conseil : s'assurer que dt soit un multiple entier de tf)
 
 # Initialisation
-    t = np.arange(0, tf + dt, dt)  # Vecteur des valeurs t_n
-    theta = np.zeros(N + 1)  # Vecteur des valeurs theta_n
-    theta[0] = 0
-    theta[1] = 0
+    t = np.linspace(0, tf, N)
+    theta = np.zeros(N)
+    theta[0] = theta0
+    theta[1] = theta0 + (1 - (beta * dt) / 2) * omega0 * dt - (g / (2 * L)) * np.sin(theta0) * dt**2
 
 # Exécution
-    for n in range(2, N + 1):
-        theta[n] = theta[n-1]
+    for n in range(1, N - 1):
+        theta[n] =  (4 * theta[n] 
+                           - (2 - beta * dt) * theta[n - 1] 
+                           - (2 * g / L) * (dt**2 / 2) * np.sin(theta[n])) / (2 + beta * dt)
+    
+    position_finale.append(theta[n-1])
 
-    thetaf[k] = theta[-1]  # Position au temps final tf
+    if firstTry:
+        plt.figure(figsize=(8, 5))
+        plt.plot(t, theta, label=r'$\theta(t)$', color='b')
+        plt.xlabel('Temps (s)')
+        plt.ylabel('Angle $\Theta$ (rad)')
+        plt.title(r'Évolution de $\Theta(t)$ pour le pendule amorti avec un pas de temps de $0,02\pi\sqrt{\frac{1}{g}}$ s')
+        plt.grid()
+        plt.show()
+        firstTry = False
+
+############################
+############################
+
+import pandas as pd
+from tabulate import tabulate
+
+data = {
+    "Pas (s)": dt_val,
+    "Position finale (m)": position_finale,
+}
+
+df = pd.DataFrame(data)
+titre = "Position du pendule au temps t=10s pour différents pas"
+
+# Affichage avec un titre
+print(f"\n{titre}\n" + "=" * len(titre))  # Affichage du titre avec soulignement
+print(tabulate(df, headers='keys', tablefmt='grid', showindex=False))
